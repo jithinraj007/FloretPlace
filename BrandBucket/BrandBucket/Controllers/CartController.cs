c01a8bd25046463a65a29f76ec55bac91a4b61d6
jithinraj007 <jithinraj.devops@gmail.com>
2021-09-19 08:12:59 +0530

    Inquiry Submission
    Verified Inquiry Details updated on database

--- BrandBucket/BrandBucket/Controllers/CartController.cs
+++ BrandBucket/BrandBucket/Controllers/CartController.cs
@@ -104,6 +104,10 @@ namespace BrandBucket.Controllers
         public async Task<IActionResult> SummaryPost(ProductUserVM ProductUserVM)
         {
 
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
             var PathToTemplate = _webHostEnvironment.WebRootPath + Path.DirectorySeparatorChar.ToString()
                 + "templates" + Path.DirectorySeparatorChar.ToString() +
                 "Inquiry.html";
@@ -134,7 +138,31 @@ namespace BrandBucket.Controllers
 
             await _emailSender.SendEmailAsync(WC.EmailAdmin, subject, messageBody);
 
-              return RedirectToAction(nameof(InquiryConfirmation));
+            InquiryHeader inquiryHeader = new InquiryHeader()
+            {
+                ApplicationUserId = claim.Value,
+                FullName = ProductUserVM.ApplicationUser.FullName,
+                Email = ProductUserVM.ApplicationUser.Email,
+                PhoneNumber = ProductUserVM.ApplicationUser.PhoneNumber,
+                InquiryDate = DateTime.Now
+            };
+            _inqHRepo.Add(inquiryHeader);
+            _inqHRepo.Save();
+
+
+            foreach (var prod in ProductUserVM.ProductList)
+            {
+                InquiryDetail inquiryDetail = new InquiryDetail()
+                {
+                    InquiryHeaderId = inquiryHeader.Id,
+                    ProductId = prod.Id
+                };
+                _inqDRepo.Add(inquiryDetail);
+            }
+            _inqDRepo.Save();
+
+
+            return RedirectToAction(nameof(InquiryConfirmation));
         }
         public IActionResult InquiryConfirmation()
         {
--- BrandBucket/BrandBucket/Startup.cs
+++ BrandBucket/BrandBucket/Startup.cs
@@ -49,6 +49,8 @@ namespace BrandBucket
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IInquiryHeaderRepository, InquiryHeaderRepository>();
             services.AddScoped<IInquiryDetailRepository, InquiryDetailRepository>();
+            services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
+
 
 
 
