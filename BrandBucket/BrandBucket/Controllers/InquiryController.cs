29692011eb1b87d0afea17696bbc097ae9765378
jithinraj007 <jithinraj.devops@gmail.com>
2021-09-19 08:45:19 +0530

    Inquiry Repository
    Created Inquiry Controller and Inquiry View Model.

---
+++ BrandBucket/BrandBucket/Controllers/InquiryController.cs
@@ -0,0 +1,27 @@
+﻿using BrandBucket_DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrandBucket.Controllers
+{
+
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+
+        public InquiryController(IInquiryDetailRepository inqDRepo, IInquiryHeaderRepository inqHRepo)
+       
+        {
+            _inqDRepo = inqDRepo;
+            _inqHRepo = _inqHRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
